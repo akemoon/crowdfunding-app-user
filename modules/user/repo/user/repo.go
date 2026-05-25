51092bfa523f7b2a284f67ff3f4c734bc795b3f0
Andrey <akhrtnv@gmail.com>
2026-05-25 15:38:53 +0300

    edit interface

--- modules/user/repo/user/repo.go
+++ modules/user/repo/user/repo.go
@@ -3,13 +3,11 @@ package user
 import (
 	"context"
 
-	lib "github.com/akemoon/crowdfunding-app-user/lib/domain"
 	"github.com/akemoon/crowdfunding-app-user/modules/user/domain"
 	"github.com/google/uuid"
 )
 
 type Repo interface {
-	CreateUser(ctx context.Context, req lib.CreateUserReq) (uuid.UUID, error)
 	GetUserByID(ctx context.Context, id uuid.UUID) (domain.User, error)
 	UpdateProfile(ctx context.Context, userID uuid.UUID, req domain.UpdateProfileReq) (domain.User, error)
 	Follow(ctx context.Context, followerID uuid.UUID, followeeID uuid.UUID) error
